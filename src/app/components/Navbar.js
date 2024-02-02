d8db2bd6bc9237fa2530b1981e35e1535ed55e68
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-02 08:18:35 +0100

    updated a simple navbar for now

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,28 +2,11 @@
 import Link from "next/link";
 import React from "react";
 
-const navLinks = [
-	{
-		title: "NFT",
-		path: "/#nft",
-	},
-	{
-		title: "Projects",
-		path: "/#projects",
-	},
-	{
-		title: "Contact",
-		path: "/#contact",
-	},
-];
-
 const Navbar = () => {
 	return (
 		<nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-80">
 			<div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-				<Link href={"/"} className="md:text-xl text-white font-semibold">
-					My Wallet{" "}
-				</Link>
+				<div className="md:text-xl text-white font-semibold">My Wallet </div>
 				<div className="menu">
 					<button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
 						settings{" "}
