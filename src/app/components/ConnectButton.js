c2d254db5c162d24dd68e38f3a2733ae370d8d1e
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-02 10:15:46 +0100

    created a simple design for the button

--- src/app/components/ConnectButton.js
+++ src/app/components/ConnectButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+// creating a component that will allow users to connect their MetaMask wallets
 const ConnectButton = () => {
-	<div>
-		<button>Connect Wallet Button</button>
-	</div>;
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<button className="text-white border-8 p-4">Connect Wallet Button</button>
+		</div>
+	);
 };
 
 export default ConnectButton;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import ConnectButton from "./components/ConnectButton";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col bg-[#121212]">
 			<Navbar />
+			<ConnectButton />
 		</main>
 	);
 }
