2b4beeadd222d2a2adb660c63ca950b38e0d3279
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2024-02-08 08:54:33 +0100

    added some comments to the MintNFT component

--- src/app/components/MintNFT.js
+++ src/app/components/MintNFT.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { ethers } from "ethers";
 
 const MintNFT = ({ signer, contract }) => {
+	//define state variable to store minting status
 	const [mintingStatus, setMintingStatus] = useState("");
 
+	//function to handle minting of NTF
+	//check if the user is connected to the BSC Testnet
 	const mintNFT = async () => {
 		try {
 			const isConnected = await signer.provider.isConnected();
@@ -12,14 +15,18 @@ const MintNFT = ({ signer, contract }) => {
 				return;
 			}
 
+			// mint NTF by calling contract's mintNFT function
 			const transaction = await contract.mintNFT({
 				value: ethers.utils.parseUnits("1", "ether"), // Adjust the fee as needed
 			});
 
+			//updating minting status
 			setMintingStatus("Waiting for confirmation...");
 
+			//waiting for transaction confirmation
 			await transaction.wait();
 
+			//updating minting status on successful minting
 			setMintingStatus("NFT Minted successfully!");
 		} catch (error) {
 			setMintingStatus(`Error minting NFT: ${error.message}`);
